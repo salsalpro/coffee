d225e7967ef109aef2c0eb7b517183c94cb221fd
salsal <to.salsal.pro@gmail.com>
2025-02-11 00:16:06 +0330

    contact and about us

--- app/about/page.js
+++ app/about/page.js
@@ -1,42 +1,42 @@
-// import React from 'react'
-// import Navbar from '@/src/components/module/Navbar/Navbar'
-// import Footer from '@/src/components/module/Footer/Footer'
-// import Coffee from '@/src/components/module/lottie/Coffee'
-// import Coffee2 from '@/src/components/module/lottie/Coffee2'
-// import styles from './styles.module.css'
+import React from 'react'
+import Navbar from '@/src/components/module/Navbar/Navbar'
+import Footer from '@/src/components/module/Footer/Footer'
+import Coffee from '@/src/components/module/lottie/Coffee'
+import Coffee2 from '@/src/components/module/lottie/Coffee2'
+import styles from './styles.module.css'
 
 
-// export default function About() {
+export default function About() {
 
-//     return (
-//         <div className={`w-full h-full ${styles.parent}`}>
-//             <Navbar with_bg={true}  />
-//             <div className='bg-red mt-74 w-full grid grid-cols-2 px-12 pb-0'>
-//                 <div className="col-span-1 flex justify-center flex-col">
-//                     <h2 className='text-white text-3xl pb-3 bg-D3AD7F p-3 rounded-t-3xl'>شروع کافی با ما </h2>
-//                     <p className='bg-D3AD7F text-lg text-white leading-8 p-3 rounded-b-3xl'>به کافی با ما خوش آمدید! ما از سال 1403 در دنیای کافه‌داری فعالیت خود را آغاز کرده‌ایم و هم‌اکنون با ارائه نوشیدنی‌های خاص و فضای دل‌نشین، به دنبال ایجاد تجربه‌ای متفاوت برای شما هستیم. تیم ما با علاقه و دقت زیاد به هنر تهیه قهوه و نوشیدنی‌های داغ و سرد می‌پردازد و در هر فنجان، سعی داریم لحظات خوشی برای شما بسازیم</p>
-//                 </div>
-//                 <div className="col-span-1 flex justify-center">
-//                     <Coffee />
-//                 </div>
-//             </div>
+    return (
+        <div className={`w-full h-full ${styles.parent}`}>
+            <Navbar with_bg={true}  />
+            <div className='bg-red mt-74 w-full grid grid-cols-2 px-12 pb-0'>
+                <div className="col-span-1 flex justify-center flex-col">
+                    <h2 className='text-white text-3xl pb-3 bg-D3AD7F p-3 rounded-t-3xl'>شروع کافی با ما </h2>
+                    <p className='bg-D3AD7F text-lg text-white leading-8 p-3 rounded-b-3xl'>به کافی با ما خوش آمدید! ما از سال 1403 در دنیای کافه‌داری فعالیت خود را آغاز کرده‌ایم و هم‌اکنون با ارائه نوشیدنی‌های خاص و فضای دل‌نشین، به دنبال ایجاد تجربه‌ای متفاوت برای شما هستیم. تیم ما با علاقه و دقت زیاد به هنر تهیه قهوه و نوشیدنی‌های داغ و سرد می‌پردازد و در هر فنجان، سعی داریم لحظات خوشی برای شما بسازیم</p>
+                </div>
+                <div className="col-span-1 flex justify-center">
+                    <Coffee />
+                </div>
+            </div>
 
-//             <div className='bg-red w-full grid grid-cols-2 px-12 pt-0'>
-//             <div className="col-span-1 flex justify-center">
-//                     <Coffee2 />
-//                 </div>
-//                 <div className="col-span-1 flex justify-center flex-col">
-//                     <h2 className='text-white text-3xl pb-3 bg-D3AD7F p-3 rounded-t-3xl'>انواع قهوه در کافی با ما</h2>
-//                     <p className='bg-D3AD7F text-lg text-white leading-8 p-3 rounded-b-3xl'>ما در کافی با ما با بهره‌گیری از بهترین دانه‌های قهوه و جدیدترین متدهای دم‌آوری، تجربه‌ای متفاوت از نوشیدن قهوه را برای شما به ارمغان می‌آوریم. چه به دنبال یک قهوه کلاسیک اسپرسو باشید و چه طرفدار نوشیدنی‌های خاص و مدرن، ما انواع قهوه‌های دلخواه شما را با طعمی بی‌نظیر و کیفیتی عالی ارائه می‌دهیم. کافیست نوع قهوه مورد علاقه‌تان را انتخاب کنید و از طعم ناب آن لذت ببرید!</p>
-//                 </div>
+            <div className='bg-red w-full grid grid-cols-2 px-12 pt-0'>
+            <div className="col-span-1 flex justify-center">
+                    <Coffee2 />
+                </div>
+                <div className="col-span-1 flex justify-center flex-col">
+                    <h2 className='text-white text-3xl pb-3 bg-D3AD7F p-3 rounded-t-3xl'>انواع قهوه در کافی با ما</h2>
+                    <p className='bg-D3AD7F text-lg text-white leading-8 p-3 rounded-b-3xl'>ما در کافی با ما با بهره‌گیری از بهترین دانه‌های قهوه و جدیدترین متدهای دم‌آوری، تجربه‌ای متفاوت از نوشیدن قهوه را برای شما به ارمغان می‌آوریم. چه به دنبال یک قهوه کلاسیک اسپرسو باشید و چه طرفدار نوشیدنی‌های خاص و مدرن، ما انواع قهوه‌های دلخواه شما را با طعمی بی‌نظیر و کیفیتی عالی ارائه می‌دهیم. کافیست نوع قهوه مورد علاقه‌تان را انتخاب کنید و از طعم ناب آن لذت ببرید!</p>
+                </div>
                 
-//             </div>
-//             <Footer />
-//         </div>
-//     );
-// }
-
-export default function About() { 
-    
-    return <h1>about</h1>
+            </div>
+            <Footer />
+        </div>
+    );
 }
+
+// export default function About() { 
+    
+//     return <h1>about</h1>
+// }
